4e45594992505b08c7ef33b01634f211e855425e
el coruco <el.coruco@gmail.com>
2016-09-10 14:16:54 -0500

    add the routes validator

---
+++ app/Http/Middleware/TypeMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TypeMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $type, $url)
+    {
+      if($request->user()->type== $type){
+        return redirect($url);
+      }
+
+      return $next($request);
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,61 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* RUTAS QUE REQUIEREN VALIDACIÓN
+ * --------------------------------------------------------------------------------
+ *
+ */
+
+Route::group(['middleware' => ['auth']], function () {
+
+  /* RUTAS DEL ADMIN
+   * --------------------------------------------------------------------------------
+   *
+   */
+  Route::group(['middleware' => 'type:admin,dashbard' ], function(){
+
+  });
+
+  /* RUTAS DE LA EMPRESA
+   * --------------------------------------------------------------------------------
+   *
+   */
+  Route::group(['middleware' => 'type:company,tablero-empresa' ], function(){
+    //
+    // AQUÍ LAS RUTAS PARA USUARIO VERIFICADO
+    //
+    Route::group(['middleware' => 'verify:tablero-empresa' ], function(){
+
+    });
+  });
+
+  /* RUTAS DE LA OPD
+   * --------------------------------------------------------------------------------
+   *
+   */
+  Route::group(['middleware' => 'type:opd,tablero-opd' ], function(){
+
+  });
+
+  /* RUTAS DEL ALUMNO
+   * --------------------------------------------------------------------------------
+   *
+   */
+  Route::group(['middleware' => 'type:student,tablero-estudiante' ], function(){
+    //
+    // AQUÍ LAS RUTAS PARA USUARIO VERIFICADO
+    //
+    Route::group(['middleware' => 'verify:tablero-estudiante' ], function(){
+
+    });
+  });
+
+  /* RUTAS DE LA CÁMARA
+   * --------------------------------------------------------------------------------
+   *
+   */
+  Route::group(['middleware' => 'type:chamber,tablero-camara' ], function(){
+
+  });
+});
