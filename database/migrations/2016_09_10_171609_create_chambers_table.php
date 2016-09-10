63827563dce1d773f7c67dfc66344cfec5b91a83
el coruco <el.coruco@gmail.com>
2016-09-10 12:36:17 -0500

    basic models

---
+++ database/migrations/2016_09_10_171609_create_chambers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChambersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chambers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('chambers');
+    }
+}
