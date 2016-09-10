a50130d57881aafe3bb738acba9544a4b9f17894
Carlos Osorio <carlos.aosorio89@gmail.com>
2016-09-10 17:06:31 -0500

    fix vacant model

--- app/models/Vacant.php
+++ app/models/Vacant.php
@@ -4,7 +4,7 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Vacant extendsModel
+class Vacant extends Model
 {
 //
 protected $fillable =['company_id','opd_id','academic_offer_id','vacant_name','vacant_location','vacant_schedule','vacant_salary','vacant_category','vacant_expiry_date','vacant_contract_type',
