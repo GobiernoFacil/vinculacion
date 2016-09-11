b7deafdc80a721ba07a7e16901e6b6610659eb9a
el coruco <el.coruco@gmail.com>
2016-09-10 19:10:14 -0500

    the routes for the user list on admin

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class Admin extends Controller
+{
+  /*
+   * D A S H B O A R D   Y   L I S T A   D E   U S U A R I O S
+   * ----------------------------------------------------------------
+   */
+
+  // El dashboard
+  //
+  //
+  public function index(){
+  }
+
+  // Todos los usuarios
+  //
+  //
+  public function users(Request $request, $page = 1){
+
+  }
+
+  // Los administradores
+  //
+  //
+  public function admins(Request $request, $page = 1){
+
+  }
+
+  // Las cámaras
+  //
+  //
+  public function chambers(Request $request, $page = 1){
+
+  }
+
+  // Las opds
+  //
+  //
+  public function opds(Request $request, $page = 1){
+
+  }
+
+  // Los estudiantes
+  //
+  //
+  public function students(Request $request, $page = 1){
+
+  }
+
+  // Las empresas
+  //
+  //
+  public function companies(Request $request, $page = 1){
+
+  }
+
+  
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// @Front controller
+// Las páginas estáticas y de consulta
+Route::get('/', "Front@index");
 Route::get('oferta-laboral/{page?}', "Front@offers");
 Route::get('oferta/{id}', "Front@offer");
 Route::get('universidades/{page?}', "Front@opds");
@@ -22,19 +21,35 @@ Route::get('universidade/{id}', "Front@opd");
 Route::get('datos-abiertos', "Front@openData");
 Route::get('privacidad', "Front@privacy");
 
+// @Suscribe controller
+// el proceso de inscripción
+Route::get('registro', "Suscribe@index");
+Route::post('registro', "Suscribe@suscribe");
+
 /* RUTAS QUE REQUIEREN VALIDACIÓN
  * --------------------------------------------------------------------------------
  *
  */
 
 Route::group(['middleware' => ['auth']], function () {
+  // @Suscribe controller
+  // una vez autorizado el usuario, se redireciona al dashboard que le corresponde
+  Route::get('guide-me', 'Suscribe@redirectToDashboard');
+
+
+
+
+
 
   /* RUTAS DEL ADMIN
    * --------------------------------------------------------------------------------
    *
    */
   Route::group(['middleware' => 'type:admin,dashbard' ], function(){
-
+    
+    // D A S H B O A R D   Y   L I S T A   D E   U S U A R I O S
+    // ----------------------------------------------------------------
+    Route::get('dashboard', 'Admin@index');
   });
 
 
