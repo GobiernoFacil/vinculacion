9e4e4c9ce021292efb1cd5e8b14e9f7431479b96
hugovom <hugo.om@gmail.com>
2016-09-13 23:17:09 -0500

    update home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -8,7 +8,23 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<h1>Encuentra tu próximo empleo</h1>
-			<p>Fácil vinculación y acceso a oportunidades laborales a estudiantes de las Universidades Politécnicas del Estado de Puebla</p>
+		</div>
+		<div class="col-sm-8 col-sm-offset-2">
+			<p class="lead">Fácil vinculación y acceso a oportunidades laborales a <strong>estudiantes</strong> de las Universidades Politécnicas del Estado de Puebla</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-4 col-sm-offset-2">
+			<div class="signup">
+				<p><strong>1200</strong> vacantes, envía tu CV</p>
+				<a>Regístrate</a>
+			</div>
+		</div>
+		<div class="col-sm-4">
+			<div class="signup company">
+				<p><strong>56</strong> sectores con talento</p>
+				<a>Publica vacante</a>
+			</div>
 		</div>
 	</div>
 </div>
