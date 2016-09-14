4f14061e598b658f6bbbefb3a1de5e5c5813d198
hugovom <hugo.om@gmail.com>
2016-09-14 12:58:02 -0500

    add dashboard view

---
+++ resources/views/companies/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master-admin')
+@section('title', 'Empleo Abierto')
+@section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
+@section('bodyclass', 'company')
+
+
+@section('content')
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Tablero de compradores</h1>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
