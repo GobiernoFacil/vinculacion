cc24db47462fd23dd9d5c67b71c46df375001c15
hugovom <hugo.om@gmail.com>
2016-09-13 22:26:56 -0500

    add home frontend

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('title', 'Empleo Abierto')
+@section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
+@section('bodyclass', 'home')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h1>Encuentra tu próximo empleo</h1>
+			<p>Fácil vinculación y acceso a oportunidades laborales a estudiantes de las Universidades Politécnicas del Estado de Puebla</p>
+		</div>
+	</div>
+</div>
+@endsection
