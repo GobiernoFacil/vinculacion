4123d1d34515e3736dd04d5e23346541d2ac78fc
hugovom <hugo.om@gmail.com>
2016-09-13 22:27:55 -0500

    update header layout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,18 @@
 <header>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-4">
+				<h2 class="sep">Secretaría de Educación Pública del Estado de Puebla</h2>
+			</div>
+			<div class="col-sm-4">
+				<h2 class="abierto"><i class="logo"></i>Empleo Abierto</h2>
+			</div>
+			<div class="col-sm-4">
+				<ul>
+					<li><a href="" class="register"><i class="material-icons">person_add</i> <span>Regístrate</span></a></li>
+					<li><a href=""><i class="material-icons">person</i> <span>Iniciar Sesión</span></a></li>
+					<li><a href="" class="vacancies"><i class="material-icons">business_center</i> <span>Publica Vacantes</span></a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
