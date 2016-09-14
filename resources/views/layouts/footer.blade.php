9f30b7871abf8802182f9ad5ee96f49efbb01d63
hugovom <hugo.om@gmail.com>
2016-09-14 02:04:00 -0500

    add footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,20 @@
 <footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<a href="http://www.sep.pue.gob.mx/" class="sep_w">Secretaría de Educación Pública del Estado de Puebla</a>
+			</div>
+			<div class="col-sm-6">
+				<p><strong>Empleo Abierto</strong> es el resultado de las mesas de trabajo sobre vinculación al mercado laboral del  Programa GAPE (gobierno abierto, participativo y eficiente) del  Estado de Puebla, que se realiza en el sector de la Educación Técnica Superior de la <a href="http://www.sep.pue.gob.mx/">Secretaría de Educación Pública del Estado de Puebla</a>.</p>
+				<p><a href="http://www.puebla.gob.mx/">Gobierno del Estado de Puebla</a>, todos los derechos reservados © 2011 - 2017</p>
+			</div>
+			<div class="col-sm-3 right">
+				<a href="http://www.bancomundial.org/es/country/mexico" class="banco">Banco Mundial</a>
+			</div>
+		</div>
+	</div>
+	<div class="author">
+		<p>Forjado Artesanalmente por</p>
+		<a href="http://gobiernofacil.com/" class="gobiernofacil">Gobierno Fácil</a>
+	</div>
 </footer>
