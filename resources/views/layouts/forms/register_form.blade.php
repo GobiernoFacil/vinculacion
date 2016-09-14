021b56a335986f731d3e32ff18dbef2ecf8053f8
hugovom <hugo.om@gmail.com>
2016-09-14 12:13:44 -0500

    update register form

--- resources/views/layouts/forms/register_form.blade.php
+++ resources/views/layouts/forms/register_form.blade.php
@@ -24,25 +24,30 @@
 	    @endif
 	</div>
 	<!--password-->
-    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-    	<input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" required>
-
-    	@if ($errors->has('password'))
-    	    <span class="help-block">
-    	        <strong>{{ $errors->first('password') }}</strong>
-    	    </span>
-    	@endif
-    </div>
-
-    <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required>
-
-        @if ($errors->has('password_confirmation'))
-            <span class="help-block">
-                <strong>{{ $errors->first('password_confirmation') }}</strong>
-            </span>
-        @endif
-    </div>
+	<div class="row">
+		<div class="col-sm-5">
+    		<div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+    			<input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" required>
+			
+    			@if ($errors->has('password'))
+    			    <span class="help-block">
+    			        <strong>{{ $errors->first('password') }}</strong>
+    			    </span>
+    			@endif
+    		</div>
+		</div>
+		<div class="col-sm-5 col-sm-offset-2">
+    		<div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+    		    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required>
+			
+    		    @if ($errors->has('password_confirmation'))
+    		        <span class="help-block">
+    		            <strong>{{ $errors->first('password_confirmation') }}</strong>
+    		        </span>
+    		    @endif
+    		</div>
+		</div>
+	</div>
 
     <!--terms-->
     <div class="form-group">
