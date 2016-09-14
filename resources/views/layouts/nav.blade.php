5716a020277f0cb8bf1e160496401ab4e6124566
hugovom <hugo.om@gmail.com>
2016-09-14 12:58:59 -0500

    update nav layout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,14 +1,17 @@
 <header>
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-3 right">
+			<div class="col-sm-4">
+				<h2 class="abierto"><i class="logo"></i>Empleo Abierto</h2>
+			</div>
+			<div class="col-sm-3 col-sm-offset-5 right">
 				<!-- Authentication Links -->
                 	<button  class="logout" onclick="location.href='/../logout'">
                 	    Cerrar Sesión
                 	</button>
                 	<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                 	    {{ csrf_field() }}
-                	</form></p>
+                	</form>
                 
 			</div>
 		</div>
