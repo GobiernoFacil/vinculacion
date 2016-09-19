6e5d8d3c38015c08d001f41c5db1730aaff8082b
hugovom <hugo.om@gmail.com>
2016-09-19 17:24:16 -0500

    add company profile view

---
+++ resources/views/companies/me_view.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master-admin')
+@section('title', 'Empleo Abierto')
+@section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
+@section('bodyclass', 'company me')
+
+@section('content')
+@endsection
