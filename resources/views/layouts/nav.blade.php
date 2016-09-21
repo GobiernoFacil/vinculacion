f13d86199737f8f51280e206519ec13b4eda77a1
hugovom <hugo.om@gmail.com>
2016-09-21 14:09:51 -0500

    fix class

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -24,7 +24,7 @@
 				<i class="material-icons">home</i> Tablero</a>
 			<a href="{{url('dashboard/administradores')}}" class="{{$__env->yieldContent('bodyclass') == 'users' ? 'current' : ''}} "><i class="material-icons">group</i> Administradores</a>
 			<a href="{{url('dashboard/opds')}}" class="{{$__env->yieldContent('bodyclass') == 'opds' ? 'current' : ''}} "><i class="material-icons">location_city</i> Universidades</a>
-			<a href="{{url('dashboard/estudiantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'opds' ? 'estudiantes' : ''}} "><i class="material-icons">school</i> Estudiantes</a>
+			<a href="{{url('dashboard/estudiantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'estudiantes' ? 'current' : ''}} "><i class="material-icons">school</i> Estudiantes</a>
 			<a href="{{url('dashboard/empresas')}}"  class="{{$__env->yieldContent('bodyclass') == 'empresas' ? 'current' : ''}} "><i class="material-icons">domain</i> Empresas</a>
 			<a href="{{url('dashboard/vacantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'vacantes' ? 'current' : ''}} "><i class="material-icons">business_center</i> Vacantes</a>
 			<a href="{{url('dashboard/yo')}}"  class="{{$__env->yieldContent('bodyclass') == 'me' ? 'current' : ''}} "><i class="material-icons">person</i> Perfil</a>
