216131c2f8ad160ea4a016ab4988f373165ba93e
hugovom <hugo.om@gmail.com>
2016-09-21 13:04:07 -0500

    update nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -20,9 +20,14 @@
 	<div class="container">
 		<div class="col-sm-12">
 			@if ($user->type == 'admin')
-			<a href="{{url('dashboard')}}" class="current"><i class="material-icons">home</i> Tablero</a>
-			<a href="#"><i class="material-icons">business_center</i> Vacantes</a>
-			<a href="{{url('dashboard/yo')}}"><i class="material-icons">domain</i> Perfil</a>
+			<a href="{{url('dashboard')}}" class="{{$__env->yieldContent('bodyclass') == 'dashboard' ? 'current' : ''}} ">
+				<i class="material-icons">home</i> Tablero</a>
+			<a href="{{url('dashboard/administradores')}}" class="{{$__env->yieldContent('bodyclass') == 'users' ? 'current' : ''}} "><i class="material-icons">group</i> Administradores</a>
+			<a href="{{url('dashboard/opds')}}" class="{{$__env->yieldContent('bodyclass') == 'opds' ? 'current' : ''}} "><i class="material-icons">location_city</i> Universidades</a>
+			<a href="{{url('dashboard/estudiantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'opds' ? 'estudiantes' : ''}} "><i class="material-icons">school</i> Estudiantes</a>
+			<a href="{{url('dashboard/empresas')}}"  class="{{$__env->yieldContent('bodyclass') == 'empresas' ? 'current' : ''}} "><i class="material-icons">domain</i> Empresas</a>
+			<a href="{{url('dashboard/vacantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'vacantes' ? 'current' : ''}} "><i class="material-icons">business_center</i> Vacantes</a>
+			<a href="{{url('dashboard/yo')}}"  class="{{$__env->yieldContent('bodyclass') == 'me' ? 'current' : ''}} "><i class="material-icons">person</i> Perfil</a>
 			@endif
 			
 			@if($user->type == 'company')
