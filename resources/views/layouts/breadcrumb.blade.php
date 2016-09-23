b393e91485d225d881bf93a16aee3bd2e97a9116
hugovom <hugo.om@gmail.com>
2016-09-22 23:18:15 -0500

    update breadcrumb

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -2,10 +2,15 @@
 	<div class="row">
 		<div class="col-sm-12 breadcrumb">
 			<ul>
-				<li><a href="{{ url('/dashboard')}}"><i class="material-icons">home</i> 
+				<li><a href="{{ url('dashboard')}}"><i class="material-icons">home</i> 
 				</a></li>
 				@if ($__env->yieldContent('breadcrumb_a') == 'user')
 				<li>Usuarios Administradores</li>
+				@endif
+				@if ($__env->yieldContent('breadcrumb_a') == 'user create')
+				<li><a href="{{ url('dashboard/administradores')}}">Usuarios Administradores
+				</a></li>
+				<li>Crear Usuario</li>
 				@endif								
 			</ul>
 		</div>
