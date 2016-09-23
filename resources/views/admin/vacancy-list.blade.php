25e79bcd48c77afd4d5e7388e4c9879aa84c5161
hugovom <hugo.om@gmail.com>
2016-09-22 23:00:33 -0500

    update vacancy view

---
+++ resources/views/admin/vacancy-list.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master-admin')
+@section('title', 'Lista de Vacantes')
+@section('description', 'Lista de Vacantes en la plataforma del Gobierno del Estado de Puebla')
+@section('bodyclass', 'vacantes')
+@section('breadcrumb', 'layouts.breadcrumb')
+@section('breadcrumb_a', 'vacantes')
+
+@section('content')
+<div class="row">
+<!-- Vacantes -->
+	<div class="col-sm-12">
+		<h1>Vacantes</h1>
+	</div>
+	<div class="col-sm-12">
+	@if($vacancies->count())
+	  <ul class="list">
+	  	<li class="row titles">
+	  	</li>
+	  @foreach($vacancies as $vacancy)
+	    <li class="row">
+	    	{{$vacancy}}
+	    </li>
+	  @endforeach
+	  </ul>
+	
+	@else
+		<p>No hay vacantes</p>
+	@endif
+	
+	
+	{{ $vacancies->links() }}
+	</div>
+</div>
+@endsection
