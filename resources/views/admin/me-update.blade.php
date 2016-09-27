6643b187bfca1ed4972a85723a7367181f65f4a6
hugovom <hugo.om@gmail.com>
2016-09-26 23:54:48 -0500

    update me view

--- resources/views/admin/me-update.blade.php
+++ resources/views/admin/me-update.blade.php
@@ -1,8 +1,15 @@
 @extends('layouts.master-admin')
+@section('title', 'Editar mi perfil')
+@section('description', 'Editar mi perfil en la plataforma de vinculación del Gobierno del Estado de Puebla')
+@section('bodyclass', 'me')
+@section('breadcrumb', 'layouts.breadcrumb')
+@section('breadcrumb_a', 'me-update')
+
 @section('content')
-<div class="container">
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
 <!-- Formulario de perfil -->
-<h4>Mi perfil</h4>
+<h2>Editar mi perfil</h2>
 
 <!-- 
      El form se pega a un objeto, y así se llenan automáticamente los inputs
@@ -20,7 +27,7 @@
 {!! Form::model($user, ['url' => 'dashboard/yo/editar', "class" => "form-horizontal"]) !!}
 
 <p>
-  <label>nombre</label>
+  <label>Nombre</label>
   {{Form::text('name', null, ["class" => "form-control"])}}
   @if($errors->has('name'))
     <strong>{{$errors->first('name')}}</strong>
@@ -28,26 +35,25 @@
 </p>
 
 <p>
-  <label>correo</label>
-  {{Form::text('email')}}
+  <label>Correo</label>
+  {{Form::text('email', null,["class" => "form-control", "disabled"])}}
   @if($errors->has('email'))
     <strong>{{$errors->first('email')}}</strong>
   @endif
 </p>
 
 <p>
-  <label>password</label>
-  {{Form::password('password', ['class' => 'la-clave-secreta'])}}
+  <label>Contraseña</label>
+  {{Form::password('password', ['class' => "form-control"])}}
   @if($errors->has('password'))
     <strong>{{$errors->first('password')}}</strong>
   @endif
 </p>
 
-<p>{{Form::submit('Actualizar')}}</p>
+<p>{{Form::submit('Actualizar', ['class' => "btn"])}}</p>
 
 <!-- se cierra el form -->
 {!! Form::close() !!}
-</div>
 </div>
 
 </div>
