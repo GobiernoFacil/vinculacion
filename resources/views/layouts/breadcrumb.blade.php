637a18d75dc9891a0c7de861a3236fa2fa1b3b04
hugovom <hugo.om@gmail.com>
2016-09-26 23:06:15 -0500

    update me profile

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -2,9 +2,9 @@
 	<div class="row">
 		<div class="col-sm-12 breadcrumb">
 			<ul>
-				<li><a href="{{ url('dashboard')}}"><i class="material-icons">home</i> 
-				</a></li>
+				<li><a href="{{ url('dashboard')}}"><i class="material-icons">home</i> </a></li>				
 				@if ($__env->yieldContent('breadcrumb_a') == 'user')
+				<!-- users -->
 				<li>Usuarios Administradores</li>
 				@endif
 				@if ($__env->yieldContent('breadcrumb_a') == 'user create')
@@ -12,6 +12,22 @@
 				</a></li>
 				<li>Crear Usuario</li>
 				@endif								
+				@if ($__env->yieldContent('breadcrumb_a') == 'opds')
+				<!-- opds -->
+				<li>Universidades</li>
+				@endif
+				@if ($__env->yieldContent('breadcrumb_a') == 'estudiantes')
+				<!-- estudiantes -->
+				<li>Estudiantes</li>
+				@endif
+				@if ($__env->yieldContent('breadcrumb_a') == 'empresas')
+				<!-- estudiantes -->
+				<li>Empresas</li>
+				@endif
+				@if ($__env->yieldContent('breadcrumb_a') == 'me')
+				<!-- me -->
+				<li>Mi Perfil</li>
+				@endif
 			</ul>
 		</div>
 	</div>
