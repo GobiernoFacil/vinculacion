35263f4a72e918377f4b74658e5d502c60c68dcd
Carlos Osorio <carlos.aosorio89@gmail.com>
2016-10-11 18:30:53 -0500

    changes add excel-students

--- resources/views/opds/students/students-add-xlsx.blade.php
+++ resources/views/opds/students/students-add-xlsx.blade.php
@@ -2,27 +2,28 @@
 @section('title', 'Actualizar estudiantes vía Excel')
 @section('description', 'Lista de Estudiantes del Gobierno del Estado de Puebla')
 @section('bodyclass', 'estudiantes')
-<?php /* 
-@section('breadcrumb', 'layouts.breadcrumb') 
+<?php /*
+@section('breadcrumb', 'layouts.breadcrumb')
 @section('breadcrumb_a', 'estudiantes')
 */ ?>
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Estudiantes</h1>
+    <h1>Agregar Estudiantes</h1>
   </div>
 
   {!! Form::open(['url' => 'tablero-opd/estudiantes/actualizar/xlsx', 'files' => true]) !!}
+  <p>Agregar varios estudiantes a través de un archivo excel</p>
     {!! csrf_field() !!}
     <p>
     {{Form::file('file')}} <br>
-    (el archivo debe ser excel menor a 2mb)
+    (El archivo debe ser excel menor a 2mb)
     </p>
     @if($errors->has('file'))
       <strong>{{$errors->first('file')}}</strong>
     @endif
 
-    <p><input type="submit" name="Enviar"></p>
+    <p><input type="submit" name="Enviar" class ="btn"></p>
   {!! Form::close() !!}
 
 </div>
