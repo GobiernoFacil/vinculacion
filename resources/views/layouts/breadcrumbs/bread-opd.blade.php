68633f88e58c33e4ac3bc9d5b4275d0a09bcdbe9
hugovom <hugo.om@gmail.com>
2016-10-11 23:01:20 -0500

    add breadcrumb to opd profile and update bodyclass

--- resources/views/layouts/breadcrumbs/bread-opd.blade.php
+++ resources/views/layouts/breadcrumbs/bread-opd.blade.php
@@ -3,8 +3,7 @@
 <li>Estudiantes</li>
 @endif
 @if ($__env->yieldContent('breadcrumb_o') == 'estudiante ver' || $__env->yieldContent('breadcrumb_o') == 'estudiante actualizar' || $__env->yieldContent('breadcrumb_o') == 'estudiante add' || $__env->yieldContent('breadcrumb_o') == 'estudiantes xls')
-<li><a href="{{ url('tablero-opd/estudiantes')}}">Estudiantes
-</a></li>
+<li><a href="{{ url('tablero-opd/estudiantes')}}">Estudiantes</a></li>
 @endif
 @if ($__env->yieldContent('breadcrumb_o') == 'estudiante ver')
 <li>Ver Estudiante</li>
@@ -24,8 +23,7 @@
 <li>Empresas</li>
 @endif
 @if ($__env->yieldContent('breadcrumb_o') == 'empresa ver' || $__env->yieldContent('breadcrumb_o') == 'empresa actualizar' || $__env->yieldContent('breadcrumb_o') == 'empresa add' || $__env->yieldContent('breadcrumb_o') == 'empresas xls')
-<li><a href="{{ url('dashboard/empresas')}}">Empresas
-</a></li>
+<li><a href="{{ url('tablero-opd/empresas')}}">Empresas</a></li>
 @endif
 @if ($__env->yieldContent('breadcrumb_o') == 'empresa ver')
 <li>Ver Empresa</li>
@@ -38,4 +36,13 @@
 @endif
 @if ($__env->yieldContent('breadcrumb_o') == 'empresas xls')
 <li>Agregar Empresas</li>
+@endif
+
+@if ($__env->yieldContent('breadcrumb_o') == 'me')
+<!-- me -->
+<li>Mi Perfil</li>
+@endif
+@if ($__env->yieldContent('breadcrumb_o') == 'me-update')
+<li><a href="{{ url('tablero-opd/yo')}}">Mi Perfil</a></li>
+<li>Editar Perfil</li>
 @endif
