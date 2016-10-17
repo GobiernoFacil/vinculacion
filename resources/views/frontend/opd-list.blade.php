3f79684c22e71303a43b0103fa5ada4ee33df7a3
hugovom <hugo.om@gmail.com>
2016-10-16 23:11:19 -0500

    update opd list

--- resources/views/frontend/opd-list.blade.php
+++ resources/views/frontend/opd-list.blade.php
@@ -1,18 +1,24 @@
 @extends('layouts.master')
 @section('title', 'Empleo Abierto')
 @section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
-@section('bodyclass', 'home')
+@section('bodyclass', 'opds')
 
 @section('content')
+<section>
 <div class="container">
-
+	<div class="col-sm-12">
+		<h1>Lista de Universidades</h1>
   <!-- el buscador -->
   {{Form::open(['url' => 'universidades', 'method' => 'get'])}}
     <p>{{Form::text('query', request('query', ''))}}</p>
   {{Form::close()}}
   <!-- La lista de maromas -->
   @if($opds->count())
-  <ul>
+  <ul class="list">
+	  <li class="titles row">
+	  	<span class="col-sm-8">Universidad</span>
+	  	<span class="col-sm-4">Ubicación</span>
+	  </li>
     @foreach($opds as $opd)
     <li><a href="{{url('universidad/' . $opd->id)}}">{{$opd->opd_name}}</a></li>
     @endforeach
@@ -23,5 +29,7 @@
 
   <!-- la paginación -->
   {{ $opds->links() }}
+	</div>
 </div>
+</section>
 @endsection
