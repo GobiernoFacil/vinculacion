d1c44692bc47bcb524970c636e527cba8e771995
Carlos Osorio <carlos.aosorio89@gmail.com>
2016-10-18 18:13:14 -0500

    changes alert message for companies

--- resources/views/companies/dashboard.blade.php
+++ resources/views/companies/dashboard.blade.php
@@ -10,7 +10,7 @@
 		<div class="row">
 			<div class="col-sm-12">
 				@if(!$user->enabled)
-				<p>Para que tu información forme parte del sitio, y puedas publicar vacantes, un tecnológico debe autorizar tu registro. Si este procedimiento tarda, puedes contactarlos directamente, buscando su información en el directorio de <strong><a target="_blank" href = "{{url('tablero-empresas/universidades')}}">universidades</a></strong>.</p>
+         @include('companies.alert-message')
 				@endif
 				<h1>Tablero de compradores</h1>
 			</div>
--- resources/views/companies/me_update.blade.php
+++ resources/views/companies/me_update.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.master-admin')
 @section('title', 'Actualizar perfil - Empleo Abierto')
 @section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
+@section('bodyclass', 'company')
 @section('content')
 <div class="container">
   <!-- Formulario de company -->
   <div class="row">
     <div class="col-sm-12">
       @if(!$user->enabled)
-      <p>Para que tu información forme parte del sitio, y puedas publicar vacantes, un tecnológico debe autorizar tu registro. Si este procedimiento tarda, puedes contactarlos directamente, buscando su información en el directorio de <strong><a target="_blank" href = "{{url('tablero-empresas/universidades')}}">universidades</a></strong>.</p>
+        @include('companies.alert-message')
       @endif
       <h1 class="separator">Editar Perfil</h1>
     </div>
