bf6de7943ebc210b27a38c18df13392a6cec3fe3
hugovom <hugo.om@gmail.com>
2016-10-23 15:14:51 -0500

    update nav @ students

--- resources/views/layouts/nav/nav_student.blade.php
+++ resources/views/layouts/nav/nav_student.blade.php
@@ -1,5 +1,7 @@
 <a href="{{url('tablero-estudiante')}}" class="{{$__env->yieldContent('bodyclass') == 'student' ? 'current' : ''}} "><i class="material-icons">home</i> Tablero</a>
 <a href="{{url('tablero-estudiante/vacantes')}}"  class="{{$__env->yieldContent('bodyclass') == 'student vacantes' ? 'current' : ''}} "><i class="material-icons">business_center</i> Vacantes</a>
+@if($user->enabled)
 <a href="{{url('tablero-estudiante/entrevistas')}}" class="{{$__env->yieldContent('bodyclass') == 'student entrevistas' ? 'current' : ''}} "><i class="material-icons">assignment</i> Entrevistas</a>
+@endif
 <a href="{{url('tablero-estudiante/cv')}}" class="{{$__env->yieldContent('bodyclass') == 'student cv' ? 'current' : ''}} "><i class="material-icons">folder</i> CV</a>
 <a href="{{url('tablero-estudiante/yo')}}"  class="{{$__env->yieldContent('bodyclass') == 'student me' ? 'current' : ''}} "><i class="material-icons">person</i> Perfil</a>
