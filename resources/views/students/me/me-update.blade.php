550a52c1d1b087a718b00b9147ed2af6a995a255
hugovom <hugo.om@gmail.com>
2016-10-23 14:41:54 -0500

    update profile @ students

--- resources/views/students/me/me-update.blade.php
+++ resources/views/students/me/me-update.blade.php
@@ -3,59 +3,56 @@
 @section('description', 'Editar mi perfil en la plataforma de vinculación del Gobierno del Estado de Puebla')
 @section('bodyclass', 'student me')
 @section('breadcrumb', 'layouts.breadcrumb')
-@section('breadcrumb_o', 'me-update')
+@section('breadcrumb_e', 'me-update')
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-      @if(!$user->enabled)
-        @include('students.alert-message')
-      @endif
-      <!-- Formulario de perfil -->
-      <h1>Editar mi perfil</h1>
-    </div>
+<div class="row">
+	<div class="col-sm-12">
+    	@if(!$user->enabled)
+			@include('students.alert-message')
+		@endif
+    <!-- Formulario de perfil -->
+    <h1>Editar mi perfil</h1>
   </div>
-  <div class="row">
-    <div class="col-sm-6 col-sm-offset-3">
-      {!! Form::model($user, ['url' => 'tablero-estudiante/yo/editar', "class" => "form-horizontal"]) !!}
+</div>
+<div class="row">
+  <div class="col-sm-6 col-sm-offset-3">
+    {!! Form::model($user, ['url' => 'tablero-estudiante/yo/editar', "class" => "form-horizontal"]) !!}
 
-      <!-- cosas del user -->
-      <fieldset>
-        <p>
-          <label>Nombre</label>
-          {{Form::text('name', !empty($user->name) ? $user->name:'',["class" => "form-control"])}}
-          @if($errors->has('name'))
-          <strong>{{$errors->first('name')}}</strong>
-          @endif
-        </p>
+    <!-- cosas del user -->
+    <fieldset>
+      <p>
+        <label>Nombre</label>
+        {{Form::text('name', !empty($user->name) ? $user->name:'',["class" => "form-control"])}}
+        @if($errors->has('name'))
+        <strong>{{$errors->first('name')}}</strong>
+        @endif
+      </p>
 
-        <p>
-          <label>Correo</label>
-          {{Form::text('email', !empty($user->email) ? $user->email : '' ,["class" => "form-control"])}}
-          @if($errors->has('email'))
-          <strong>{{$errors->first('email')}}</strong>
-          @endif
-        </p>
+      <p>
+        <label>Correo</label>
+        {{Form::text('email', !empty($user->email) ? $user->email : '' ,["class" => "form-control"])}}
+        @if($errors->has('email'))
+        <strong>{{$errors->first('email')}}</strong>
+        @endif
+      </p>
 
-        <p>
-          <label>Contraseña</label>
-          {{Form::password('password',["class" => "form-control"])}}
-          @if($errors->has('password'))
-          <strong>{{$errors->first('password')}}</strong>
-          @endif
-        </p>
-      </fieldset>
-
-
+      <p>
+        <label>Contraseña</label>
+        {{Form::password('password',["class" => "form-control"])}}
+        @if($errors->has('password'))
+        <strong>{{$errors->first('password')}}</strong>
+        @endif
+      </p>
+    </fieldset>
 
 
-      <p>{{Form::submit('Actualizar', ['class' => "btn"])}}</p>
 
-      <!-- se cierra el form -->
-      {!! Form::close() !!}
-    </div>
 
+    <p>{{Form::submit('Actualizar', ['class' => "btn"])}}</p>
+
+    <!-- se cierra el form -->
+    {!! Form::close() !!}
   </div>
 </div>
 @endsection
