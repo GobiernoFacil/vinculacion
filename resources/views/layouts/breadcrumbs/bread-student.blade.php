aa02ba705818a73b317478184b53bfe92365687b
hugovom <hugo.om@gmail.com>
2016-10-22 23:33:31 -0500

    update breadcrumb

--- resources/views/layouts/breadcrumbs/bread-student.blade.php
+++ resources/views/layouts/breadcrumbs/bread-student.blade.php
@@ -1,32 +1,32 @@
 
-@if ($__env->yieldContent('breadcrumb_o') == 'vacantes')
+@if ($__env->yieldContent('breadcrumb_e') == 'vacantes')
 <!-- vacancies -->
 <li>Vacantes</li>
 @endif
-@if ($__env->yieldContent('breadcrumb_o') == 'vacante ver')
+@if ($__env->yieldContent('breadcrumb_e') == 'vacante ver')
 <li><a href="{{ url('tablero-estudiante/vacantes')}}">Vacantes</a></li>
 @endif
-@if ($__env->yieldContent('breadcrumb_o') == 'vacante ver')
+@if ($__env->yieldContent('breadcrumb_e') == 'vacante ver')
 <li>Ver Vacante</li>
 @endif
 
 
-@if ($__env->yieldContent('breadcrumb_o') == 'entrevistas')
+@if ($__env->yieldContent('breadcrumb_e') == 'entrevistas')
 <!-- interviews -->
 <li>Entrevistas</li>
 @endif
-@if ($__env->yieldContent('breadcrumb_o') == 'entrevista ver')
+@if ($__env->yieldContent('breadcrumb_e') == 'entrevista ver')
 <li><a href="{{ url('tablero-estudiante/entrevistas')}}">Entrevistas</a></li>
 @endif
-@if ($__env->yieldContent('breadcrumb_o') == 'entrevista ver')
+@if ($__env->yieldContent('breadcrumb_e') == 'entrevista ver')
 <li>Ver Entrevista</li>
 @endif
 
-@if ($__env->yieldContent('breadcrumb_o') == 'me')
+@if ($__env->yieldContent('breadcrumb_e') == 'me')
 <!-- me -->
 <li>Mi Perfil</li>
 @endif
-@if ($__env->yieldContent('breadcrumb_o') == 'me-update')
+@if ($__env->yieldContent('breadcrumb_e') == 'me-update')
 <li><a href="{{ url('tablero-estudiante/yo')}}">Mi Perfil</a></li>
 <li>Editar Perfil</li>
 @endif
