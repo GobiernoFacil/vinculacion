c6f1aa1f1b5cfb69e9a81399b390711265325ee9
hugovom <hugo.om@gmail.com>
2016-10-23 14:39:04 -0500

    update profile view @ students

--- resources/views/students/me/me.blade.php
+++ resources/views/students/me/me.blade.php
@@ -3,27 +3,31 @@
 @section('description', 'Mi perfil en la plataforma de vinculación del Gobierno del Estado de Puebla')
 @section('bodyclass', 'student me')
 @section('breadcrumb', 'layouts.breadcrumb')
-@section('breadcrumb_o', 'me')
+@section('breadcrumb_e', 'me')
 
 @section('content')
 <div class="row">
-	<div class="col-sm-8 col-sm-offset-2">
-		<h3>Mi Perfil</h3>
-	</div>
-	<div class="col-sm-8 col-sm-offset-2">
-		@if(!$user->enabled)
+	<div class="col-sm-12">
+	@if(!$user->enabled)
 			@include('students.alert-message')
 		@endif
+	</div>
+	<div class="col-sm-12">
+		<h1>Mi Perfil</h1>
+	</div>
+	<div class="col-sm-8 col-sm-offset-2">
 		<!-- Perfil -->
-		<h2>{{$student->user->name ? $student->user->name : 'Sin información' }}</h2>
-		<p><strong>Correo</strong>: {{$student->user->email ? $student->user->email : "Sin información"}}</p>
-		<p><strong>Carrera</strong>: {{$student->carrera ? $student->carrera : "Sin información"}}</p>
-		<p><strong>CURP</strong>: {{$student->curp ? $student->curp : "Sin información"}}</p>
+		<h2><i class="material-icons">person</i> {{$student->user->name ? $student->user->name : 'Sin información' }} {{$student->user->name == '(ಠ_ಠ) mi nombre es...' ? 'Agrega tu nombre' : ''}}</h2>
+		<ul class="list_perks">
+			<li><strong>Carrera</strong>: {{$student->carrera ? $student->carrera : "Sin información"}}</li>
+			<li><strong>Universidad</strong>: {{$student->opd->opd_name}}</li>
+			<li><strong>Correo</strong>: {{$student->user->email ? $student->user->email : "Sin información"}}</li>
+			<li><strong>CURP</strong>: {{$student->curp ? $student->curp : "Sin información"}}</li>
+		</ul>
 		<p>Actualizado: {{date('d-m-Y', strtotime($student->updated_at))}}</p>
 	</div>
 	<div class="col-sm-3 col-sm-offset-2">
 		<p><a href="{{url('tablero-estudiante/yo/editar')}}" class="btn">Editar Perfil</a></p>
 	</div>
 </div>
-
 @endsection
