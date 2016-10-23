d8d5e8824f96f53bc4d0326f51b25bc0184c6114
hugovom <hugo.om@gmail.com>
2016-10-22 19:56:08 -0500

    update dashboard @ students

--- resources/views/students/dashboard.blade.php
+++ resources/views/students/dashboard.blade.php
@@ -5,57 +5,55 @@
 
 
 @section('content')
-<section>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				@if(!$user->enabled)
-	        @include('students.alert-message')
-	      @endif
-				<h1>Tu Tablero</h1>
-			</div>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			@if(!$user->enabled)
+				@include('students.alert-message')
+			@endif
+			<h1>Tu Tablero</h1>
 		</div>
-		<!--perfil-->
-		<div class="row">
-			<div class="col-sm-8">
-				<div class="box">
-					<div class="col-sm-4">
-						<div class="figure">
-							<i class="material-icons">location_city</i>
-						</div>
+	</div>
+	<!--perfil-->
+	<div class="row">
+		<div class="col-sm-8">
+			<div class="box">
+				<div class="col-sm-4">
+					<div class="figure">
+						<i class="material-icons">location_city</i>
 					</div>
-					<div class="col-sm-8">
-						<h3>{{$user->name}}</h3>
-						<p>{{$user->student->matricula}}</p>
-            <p>{{$user->student->carrera}}</p>
-						<ul class="listinfo">
-							@if(!empty($user->email))
-							<li><strong>email</strong>: <a href="mailto:{{$user->email}}">{{$user->email}}</a></li>
-							@endif
-						</ul>
-
-						<p><a class="btn edit" href ="{{url('tablero-estudiante/yo/editar')}}">Edita tu perfil</a></p>
-					</div>
-					<div class="clearfix"></div>
 				</div>
-			</div>
-		</div>
-		<!--vacantes-->
-		<div class="row">
+				<div class="col-sm-8">
+					<h3>{{$user->name}}</h3>
+					<p>{{$user->student->matricula}}</p>
+        <p>{{$user->student->carrera}}</p>
+					<ul class="listinfo">
+						@if(!empty($user->email))
+						<li><strong>email</strong>: <a href="mailto:{{$user->email}}">{{$user->email}}</a></li>
+						@endif
+					</ul>
 
-			<div class="col-sm-4">
-				<a class="box" href="{{url('tablero-estudiante/vacantes')}}">
-					<span>Tus Vacantes</span>
-					<span class="count">{{$vacancies}}</span>
-				</a>
-			</div>
-			<div class="col-sm-4">
-				<a class="box" href="{{url('tablero-estudiante/entrevistas')}}">
-					<span>Tus Entrevistas</span>
-					<span class="count">{{$interviews}}</span>
-				</a>
+					<p><a class="btn edit" href ="{{url('tablero-estudiante/yo/editar')}}">Edita tu perfil</a></p>
+				</div>
+				<div class="clearfix"></div>
 			</div>
 		</div>
 	</div>
-</section>
+	<!--vacantes-->
+	<div class="row">
+
+		<div class="col-sm-4">
+			<a class="box" href="{{url('tablero-estudiante/vacantes')}}">
+				<span>Tus Vacantes</span>
+				<span class="count">{{$vacancies}}</span>
+			</a>
+		</div>
+		<div class="col-sm-4">
+			<a class="box" href="{{url('tablero-estudiante/entrevistas')}}">
+				<span>Tus Entrevistas</span>
+				<span class="count">{{$interviews}}</span>
+			</a>
+		</div>
+	</div>
+</div>
 @endsection
