c597874c916cdecb418972d5835eede3e6fe03c6
hugovom <hugo.om@gmail.com>
2016-10-29 22:51:53 -0500

    update interview @ companies

--- resources/views/companies/vacancies/interview.blade.php
+++ resources/views/companies/vacancies/interview.blade.php
@@ -3,27 +3,42 @@
 @section('description', 'Agregar nueva vacante en plataforma de Gobierno del Estado de Puebla')
 @section('bodyclass', 'company vacantes')
 @section('breadcrumb', 'layouts.breadcrumb')
-@section('breadcrumb_o', 'vacante add')
+@section('breadcrumb_c', 'vacante entrevista ver')
 
 @section('content')
 
   <!-- Formulario de vacante -->
   <div class="row">
     <div class="col-sm-12">
-      <h1 class="separator">Entrevista</h1>
+      <h1>Entrevista</h1>
     </div>
   </div>
+  <div class="row">
+  	<div class="col-sm-6">
+	  	<h3>Vacante: <a href="{{ url('tablero-empresa/vacante/'. $vacancy->id) }}">{{$vacancy->job}}</a></h3>
+  	</div>
+  	<div class="col-sm-6">
+		<h3>Aspirante: {{ucwords(strtolower($interview->student->nombre . ' ' . $interview->student->apellido_paterno))}}</h3>
+  	</div>
+  </div>
+  
+  <div class="separator"></div>
   
   <div class="row">
     <div class="col-sm-6 col-sm-offset-3">
-      <p>vacante: {{$vacancy->job}}</p>
-      <p>aspirante: {{$interview->student->nombre}}</p>
-
-      <p>contacto: {{$interview->contact}}</p>
-      <p>correo: {{$interview->email}}</p>
-      <p>teléfono:{{$interview->phone}}</p>
-      <p>dirección: {{$interview->address}}</p>
-      
+		<ul class="list_perks">
+			<li><strong>Carrera</strong>: {{$interview->student->carrera}}</li>
+			<li><strong>Universidad</strong>: {{$interview->student->opd->opd_name}}</li>
+			<li><strong>CV</strong>: </li>
+			<li><strong>Fecha de creación de entrevista</strong>: {{ date('d-m-Y', strtotime($interview->created_at))}}</li>
+		</ul>
+		<h3>Contacto</h3>
+		<ul class="list_perks">
+			<li><strong>Contacto</strong>: {{$interview->contact}}</li>
+			<li><strong>Correo</strong>: {{$interview->email}}</li>
+			<li><strong>Teléfono</strong>:{{$interview->phone}}</li>
+			<li><strong>Dirección</strong>: {{$interview->address}}</li>
+		</ul>
       <!--
       <p>día: {{$interview->date}}</p>
       <p>hora: {{$interview->time}}</p>
