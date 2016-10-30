bb133cfa343f67438e29e6e961f8a758c78c7212
hugovom <hugo.om@gmail.com>
2016-10-29 22:18:56 -0500

    fix #47

--- resources/views/companies/vacancies/vacancy-applicant.blade.php
+++ resources/views/companies/vacancies/vacancy-applicant.blade.php
@@ -16,13 +16,18 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-6 col-sm-offset-3">
-      <p>vacante: {{$vacancy->job}}</p>
-
-      <h4>aspirante</h4>
-      <p>{{$student->nombre}}</p>
-
-      <p><a href="{{url("tablero-empresa/vacante/{$vacancy->id}/entrevista/crear/{$student->id}")}}">Agendar una entrevista</a></p>
+    <div class="col-sm-8 col-sm-offset-2">
+      <h2>{{ucwords(strtolower($applicant->student->nombre . ' ' . $applicant->student->apellido_paterno))}}</h2>
+	  <h4>Vacante</h4>
+	  <p><a href="{{ url('tablero-empresa/vacante/'.$vacancy->id)}}">{{$vacancy->job}}</a></p>
+      <h4>Información del aspirante</h4>
+	  <ul class="list_perks">
+		  <li><strong>Carrera</strong>: {{$applicant->student->carrera}}</li>
+		  <li><strong>Universidad</strong>: {{$applicant->student->opd->opd_name}}</li>
+		  <li><strong>CV</strong>: </li>
+	  </ul>
+	  
+      <p><a href="{{url("tablero-empresa/vacante/{$vacancy->id}/entrevista/crear/{$student->id}")}}" class="btn">Agendar una entrevista</a></p>
     </div>
   </div>
 @endsection
