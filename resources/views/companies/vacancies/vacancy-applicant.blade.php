46299492575901508fe72fe683e9eeb6d0fa5d64
el coruco <el.coruco@gmail.com>
2016-10-30 19:37:42 -0600

    add cv info on company-applicant

--- resources/views/companies/vacancies/vacancy-applicant.blade.php
+++ resources/views/companies/vacancies/vacancy-applicant.blade.php
@@ -24,7 +24,35 @@
 	  <ul class="list_perks">
 		  <li><strong>Carrera</strong>: {{$applicant->student->carrera}}</li>
 		  <li><strong>Universidad</strong>: {{$applicant->student->opd->opd_name}}</li>
-		  <li><strong>CV</strong>: </li>
+		  <li><strong>CV</strong>: 
+        <ul>
+          <li>Sexo : {{$student->cv->gender}}</li>
+          <li>Edad : {{$student->cv->age}}</li>
+          <li>ciudad : {{$student->cv->city}}</li>
+          <li>estado : {{$student->cv->state}}</li>
+          <li>país : {{$student->cv->country}}</li>
+          <li>teléfono : {{$student->cv->phone}}</li>
+          <li>celular : {{$student->cv->mobile}}</li>
+          <li>correo : {{$student->cv->email}}</li>
+          <li>
+            <h5>idiomas</h5>
+            <ul>
+            @foreach($student->cv->languages as $language)
+              <li>{{$language->name}} : {{$language->level}}</li>
+            @endforeach
+            </ul>
+          </li>
+
+          <li>
+            <h5>software</h5>
+            <ul>
+            @foreach($student->cv->softwares as $software)
+              <li>{{$software->name}} : {{$software->level}}</li>
+            @endforeach
+            </ul>
+          </li>
+        </ul>
+      </li>
 	  </ul>
 	  
       <p><a href="{{url("tablero-empresa/vacante/{$vacancy->id}/entrevista/crear/{$student->id}")}}" class="btn">Agendar una entrevista</a></p>
