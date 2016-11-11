aee19178df88629cf295538c83919c785d46dc19
hugovom <hugo.om@gmail.com>
2016-11-11 13:43:11 -0600

    update vacancies list #44

--- resources/views/students/vacancies/vacancies_applied.blade.php
+++ resources/views/students/vacancies/vacancies_applied.blade.php
@@ -3,7 +3,7 @@
 @section('description', 'Empleo Abierto del Gobierno del Estado de Puebla')
 @section('bodyclass', 'student vacantes')
 @section('breadcrumb', 'layouts.breadcrumb')
-@section('breadcrumb_e', 'vacantes')
+@section('breadcrumb_e', 'vacantes aplicadas')
 
 @section('content')
 <div class="row">
@@ -16,13 +16,13 @@
         @if(!$user->enabled)
          @include('companies.alert-message')
         @endif
-        <h1>Vacantes: {{$vacancies->count()}} </h1>
+        <h1>Vacantes Aplicadas: {{$applications->count()}} </h1>
     </div>
-    <div class="col-sm-3 col-sm-offset-9">
-    	<a href="{{ url('tablero-estudiante/vacantes-aplicadas') }}" class="btn">Ver tus vacantes aplicadas &gt;</a>
+    <div class="col-sm-3">
+    	<a href="{{ url('tablero-estudiante/vacantes') }}" class="btn">&lt; Ver todas las vacantes</a>
     </div>
     <div class="col-sm-12">
-      @if($vacancies->count())
+      @if($applications->count())
       <ul class="list">
         <li class="clearfix titles">
           <span class="col-sm-4 col-xs-4">Vacante</span>
@@ -31,20 +31,21 @@
           <span class="col-sm-2 nomobile">Ubicación</span>
           <span class="col-sm-2 nomobile">Acciones</span>
         </li>
-        @foreach($vacancies as $vacancy)
+        @foreach($applications as $application)
+        {{$application}}
         <li class="clearfix">
           <span class="col-sm-4 col-xs-4">
-          	<a href="{{url("tablero-estudiante/vacante/{$vacancy->id}")}}"> {{$vacancy->job}}</a>
+          	<a href="{{url("tablero-estudiante/vacante/{$application->vacant_id}")}}"> {{$application->vacancy['job']}}</a>
           	<br>
-          	<span class="note">Carrera: {{$vacancy->tags}}</span>
+          	<span class="note">Carrera: {{$application->tags}}</span>
           </span>
-          <span class="col-sm-2 col-xs-4">{{$vacancy->salary ? '$' .  number_format($vacancy->salary,2, '.', ',') : ''}}</span>
-          <span class="col-sm-2 col-xs-4">{{$vacancy->company->nombre_comercial}}</span>
-          <span class="col-sm-2 nomobile">{{$vacancy->city}}</span>
+          <span class="col-sm-2 col-xs-4">{{$application->salary ? '$' .  number_format($application->salary,2, '.', ',') : ''}}</span>
+          <span class="col-sm-2 col-xs-4">{{$application->company}}</span>
+          <span class="col-sm-2 nomobile">{{$application->city}}</span>
           <span class="col-sm-2  col-xs-12 right">
-            <a href="{{url("tablero-estudiante/vacante/{$vacancy->id}")}}" class="btn edit xs">Ver</a>
+            <a href="{{url("tablero-estudiante/vacante/{$application->vacant_id}")}}" class="btn edit xs">Ver</a>
             @if($user->enabled)
-				<a href="{{url("tablero-estudiante/vacante/aplicar/{$vacancy->id}")}}" class="btn xs">Aplicar</a>
+				<a href="{{url("tablero-estudiante/vacante/declinar/{$application->vacant_id}")}}" class="btn xs">Declinar</a>
             @endif
           </span>
         </li>
@@ -52,7 +53,7 @@
       </ul>
 
       @else
-    	<p>No hay ninguna vacante publicada</p>
+    	<p>No hay vacantes aplicadas.</p>
       @endif
 
     </div>
