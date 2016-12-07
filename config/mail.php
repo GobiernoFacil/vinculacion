cdac275290b364914a2bd075ee7c1fe79be7ee65
Boris Cuapio Morales <el.coruco@gmail.com>
2016-12-07 15:46:27 -0600

    add default email to the app

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => "empleouniversitario@puebla.gob.mx", 'name' => "Empleo universitario"],
 
     /*
     |--------------------------------------------------------------------------
