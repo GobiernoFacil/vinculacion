f7513ced226064442b929a14d4f3e7472bd11d3e
Boris Cuapio Morales <el.coruco@gmail.com>
2016-12-08 12:33:05 -0600

    fix routing after recover pass

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -20,6 +20,7 @@ class PasswordController extends Controller
 
     use ResetsPasswords;
 
+    protected $redirectTo = 'guide-me';
     /**
      * Create a new password controller instance.
      *
