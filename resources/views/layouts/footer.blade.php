bc7c093afbf6520734a8f75e2870b2e64b1f045e
hugovom <hugo.om@gmail.com>
2017-03-25 10:34:12 -0600

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
 			</div>
 			<div class="col-sm-6">
 				<p><strong>Empleo Universitario</strong> es el resultado de las mesas de trabajo sobre vinculación al mercado laboral del  Programa GAPE (gobierno abierto, participativo y eficiente) del  Estado de Puebla, que se realiza en el sector de la Educación Técnica Superior de la <a href="http://www.sep.pue.gob.mx/">Secretaría de Educación Pública del Estado de Puebla</a>.</p>
-				<p><a href="http://www.puebla.gob.mx/">Gobierno del Estado de Puebla</a>, todos los derechos reservados © 2011 - 2017</p>
+				<p><a href="http://www.puebla.gob.mx/">Gobierno del Estado de Puebla</a>, todos los derechos reservados © 2017 - 2018</p>
 			</div>
 			<div class="col-sm-3 right">
 				<a href="http://www.bancomundial.org/es/country/mexico" class="banco">Banco Mundial</a>
